68158d274ab1f244e5403c053f501bba6577d0f4
gurpreetgarchey-web <gurpreetgarchey@gmail.com>
2026-07-21 14:46:33 -0400

    Add files via upload

---
+++ src/test/java/listeners/ExtentListener.java
@@ -0,0 +1,41 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import utils.ExtentManager;
+
+public class ExtentListener implements ITestListener {
+
+    ExtentReports extent = ExtentManager.getExtent();
+    ExtentTest test;
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.pass("PASS");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.skip("SKIPPED");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
